e882205a0f1dddc3e12fd7db5e34ae12709eb9fe
Haneesh Jallipalli <55525003+haneeshjallipalli@users.noreply.github.com>
2025-05-09 20:20:43 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,19 +1,14 @@
-#import flask
+from flask import Flask, jsonify
 
-from flask import Flask
-
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define the home route
-@app.route("/bilal")
+@app.route('/')
 def home():
-    return "<h1>Welcome to My Flask App! BILAL </h1><p>This is a simple Flask application.</p>"
+    return jsonify(message="Hello, this is the root endpoint!")
 
-@app.route("/haneesh")
-def functionality():
-    return "<h1>Welcome to the functionality</h1>"
+@app.route('/health')
+def health():
+    return jsonify(status="OK", message="App is healthy!")
 
-# Run the application
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
